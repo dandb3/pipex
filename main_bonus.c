f941b32eabbc655d24ba6b65ccdfe49095e86c4e
dandb3 <dandb3@github.com>
2023-01-18 20:48:42 +0900

    2

--- main_bonus.c
+++ main_bonus.c
@@ -6,16 +6,17 @@
 /*   By: jdoh <jdoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 11:53:41 by jdoh              #+#    #+#             */
-/*   Updated: 2023/01/18 19:01:19 by jdoh             ###   ########.fr       */
+/*   Updated: 2023/01/18 20:46:50 by jdoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
+#include <stdlib.h>
 
 int	main(int argc, char *argv[], char *envp[])
 {
 	if (argc < 5)
-		return (1);
+		exit(error_msg("please input arguments more than 4", 1));
 	if (pipex(argc, argv, envp) == FAILURE)
 		return (1);
 	return (0);
